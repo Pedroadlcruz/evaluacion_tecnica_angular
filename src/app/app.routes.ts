2abd8bd4f9f33cda6b3e3f9020a19308cb2c00e4
Pedro A. De la Cruz <pedrodlcruz@coolhunterpartners.com>
2025-12-10 00:48:15 -0400

    ⚡️ feat(routes): update routing to use loadComponent for lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,20 @@
 import { Routes } from '@angular/router';
-import { FavoritesPageComponent } from './favorites/pages/favorites-page/favorites-page';
-import { PokemonListPageComponent } from './pokemon/pages/pokemon-list-page/pokemon-list-page';
 
 export const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'pokemon'
-  },
+  { path: '', pathMatch: 'full', redirectTo: 'pokemon' },
   {
     path: 'pokemon',
-    component: PokemonListPageComponent
+    loadComponent: () =>
+      import('./pokemon/pages/pokemon-list-page/pokemon-list-page').then(
+        (m) => m.PokemonListPageComponent
+      ),
   },
   {
     path: 'favoritos',
-    component: FavoritesPageComponent
-  }
+    loadComponent: () =>
+      import('./favorites/pages/favorites-page/favorites-page').then(
+        (m) => m.FavoritesPageComponent
+      ),
+  },
+  { path: '**', redirectTo: 'pokemon' },
 ];
